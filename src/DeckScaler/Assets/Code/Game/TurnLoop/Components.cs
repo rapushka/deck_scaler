6a3a7fbfda54d2a5b984a444d0ecb29b76d9acf7
rapuska <mikalajsiatkouski@gmail.com>
2024-11-22 19:36:54 +0300

    OnEndTurnAllTeammatesAttackOpponents

--- src/DeckScaler/Assets/Code/Game/TurnLoop/Components.cs
+++ src/DeckScaler/Assets/Code/Game/TurnLoop/Components.cs
@@ -3,4 +3,6 @@ using Entitas.Generic;
 namespace DeckScaler.Component
 {
     public sealed class EndTurn : FlagComponent, IInScope<Game> { }
+
+    public sealed class Attack : ValueComponent<EntityID>, IInScope<Game> { }
 }
