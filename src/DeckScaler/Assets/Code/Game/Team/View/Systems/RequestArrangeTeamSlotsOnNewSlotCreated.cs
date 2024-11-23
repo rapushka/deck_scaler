92fa57e8577e85f22c793bb360f1a33e4471a2c3
rapuska <mikalajsiatkouski@gmail.com>
2024-11-23 13:27:00 +0300

    fix

---
+++ src/DeckScaler/Assets/Code/Game/Team/View/Systems/RequestArrangeTeamSlotsOnNewSlotCreated.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using DeckScaler.Component;
+using Entitas;
+using Entitas.Generic;
+using static Entitas.Generic.ScopeMatcher<DeckScaler.Game>;
+
+namespace DeckScaler.Systems
+{
+    public sealed class RequestArrangeTeamSlotsOnNewSlotCreated : ReactiveSystem<Entity<Game>>
+    {
+        public RequestArrangeTeamSlotsOnNewSlotCreated() : base(Contexts.Instance.Get<Game>()) { }
+
+        protected override ICollector<Entity<Game>> GetTrigger(IContext<Entity<Game>> context)
+            => context.CreateCollector(Get<TeamSlot>().AddedOrRemoved());
+
+        protected override bool Filter(Entity<Game> entity) => true;
+
+        protected override void Execute(List<Entity<Game>> teamSlots)
+        {
+            if (teamSlots.Any())
+                CreateEntity.OneFrame().Add<Component.ArrangeTeamSlots>();
+        }
+    }
+}
