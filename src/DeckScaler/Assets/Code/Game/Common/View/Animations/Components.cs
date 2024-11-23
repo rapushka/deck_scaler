8a365bb8b771d2fd0f1c0b1c524eb6b0687ec244
rapuska <mikalajsiatkouski@gmail.com>
2024-11-23 12:47:52 +0300

    Easings

---
+++ src/DeckScaler/Assets/Code/Game/Common/View/Animations/Components.cs
@@ -0,0 +1,10 @@
+using Entitas.Generic;
+using UnityEngine;
+
+namespace DeckScaler.Component
+{
+    /// Local Position
+    public sealed class TargetPosition : ValueComponent<Vector2>, IInScope<Game> { }
+
+    public sealed class Easing : ValueComponent<AnimationCurve>, IInScope<Game> { }
+}
--- src/DeckScaler/Assets/Code/Game/Common/View/Animations/Systems/UpdateTargetPosition.cs
+++ src/DeckScaler/Assets/Code/Game/Common/View/Animations/Systems/UpdateTargetPosition.cs
@@ -4,11 +4,14 @@ using DeckScaler.Utils;
 using DG.Tweening;
 using Entitas;
 using Entitas.Generic;
+using UnityEngine;
 
 namespace DeckScaler.Systems
 {
     public class UpdateTargetPosition : IExecuteSystem
     {
+        private static readonly AnimationCurve LinearEasing = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
         private readonly IGroup<Entity<Game>> _entities
             = Contexts.Instance.GetGroup(
                 MatcherBuilder<Game>
@@ -23,11 +26,14 @@ namespace DeckScaler.Systems
             foreach (var entity in _entities.GetEntities(_buffer))
             {
                 var z = entity.GetOrDefault<ZOrder, float>();
+                var easing = entity.GetOrDefault<Easing, AnimationCurve>(LinearEasing);
 
                 var transform = entity.Get<ViewTransform>().Value;
                 var targetPosition = entity.Get<TargetPosition>().Value;
 
-                transform.DOMove(targetPosition.Extend(z), duration: 0.3f); // TODO: duration
+                transform.DOMove(targetPosition.Extend(z), duration: 0.3f) // TODO: duration
+                         .SetEase(easing)
+                    ;
 
                 entity.Remove<TargetPosition>();
             }
