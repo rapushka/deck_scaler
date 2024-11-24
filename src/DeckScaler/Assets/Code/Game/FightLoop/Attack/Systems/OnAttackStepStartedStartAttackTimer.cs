d25d37b4c705450fd30e50942bf720916eb8a873
rapuska <mikalajsiatkouski@gmail.com>
2024-11-24 11:17:39 +0300

    delay

---
+++ src/DeckScaler/Assets/Code/Game/FightLoop/Attack/Systems/OnAttackStepStartedStartAttackTimer.cs
@@ -0,0 +1,40 @@
+using DeckScaler.Component;
+using DeckScaler.Service;
+using DeckScaler.Utils;
+using Entitas;
+using Entitas.Generic;
+
+namespace DeckScaler.Systems
+{
+    public class OnAttackStepStartedStartAttackTimer<TEventComponent, THeldComponent> : IExecuteSystem
+        where TEventComponent : FlagComponent, IInScope<Game>, new()
+        where THeldComponent : ValueComponent<EntityID>, IInScope<Game>, new()
+    {
+        private readonly IGroup<Entity<Game>> _event = Contexts.Instance.GetGroup(
+            MatcherBuilder<Game>
+                .With<TEventComponent>()
+                .Build()
+        );
+
+        private readonly IGroup<Entity<Game>> _slots = Contexts.Instance.GetGroup(
+            MatcherBuilder<Game>
+                .With<TeamSlot>()
+                .Build()
+        );
+
+        private static float DelayBetweenAttacks => Services.Get<IConfigs>().Units.DelayBetweenAttacks;
+
+        public void Execute()
+        {
+            foreach (var _ in _event)
+            foreach (var (slot, index) in _slots.GetTeamSlotsInOrder())
+            {
+                if (slot.TryGet<THeldComponent, EntityID>(out var unitID))
+                {
+                    var unit = unitID.GetEntity();
+                    unit.Add<TimerBeforeAttack, Timer>(new Timer(index * DelayBetweenAttacks));
+                }
+            }
+        }
+    }
+}
