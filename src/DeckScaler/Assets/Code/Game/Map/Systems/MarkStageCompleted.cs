ea6a4df68b150450548a8392af7eec18ef87b886
rapuska <mikalajsiatkouski@gmail.com>
2024-12-21 12:37:53 +0300

    mark stages as completed

--- src/DeckScaler/Assets/Code/Game/Map/Systems/MarkStageCompleted.cs
+++ src/DeckScaler/Assets/Code/Game/Map/Systems/MarkStageCompleted.cs
@@ -18,10 +18,31 @@ namespace DeckScaler.Systems
 
         private static ProgressData Progress => ServiceLocator.Resolve<IProgress>().CurrentRun;
 
+        private static Entity<Game> CurrentStageEntity => Contexts.Instance.Get<Game>().Unique.GetEntity<CurrentStage>();
+
+        private static PrimaryEntityIndex<Game, StageIndex, int> Index
+            => Contexts.Instance.Get<Game>().GetPrimaryIndex<StageIndex, int>();
+
         public void Execute()
         {
             foreach (var _ in _events)
+            {
                 Progress.MarkStageAsCompleted();
+
+                var currentStageEntity = CurrentStageEntity;
+                currentStageEntity
+                    .Is<CompletedStage>(true)
+                    .Is<CurrentStage>(false)
+                    ;
+
+                var previousStageIndex = currentStageEntity.Get<StageIndex, int>();
+                if (Index.TryGetEntity(previousStageIndex + 1, out var nextStage))
+                {
+                    nextStage
+                        .Is<CurrentStage>(true)
+                        ;
+                }
+            }
         }
     }
 }
