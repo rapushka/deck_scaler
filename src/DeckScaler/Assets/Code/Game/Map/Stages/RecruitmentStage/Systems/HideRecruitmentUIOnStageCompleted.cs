f65780cb9469870aa6de0fbd2be06a41a3aaa947
rapuska <mikalajsiatkouski@gmail.com>
2024-12-25 15:04:38 +0300

    skip stage button

---
+++ src/DeckScaler/Assets/Code/Game/Map/Stages/RecruitmentStage/Systems/HideRecruitmentUIOnStageCompleted.cs
@@ -0,0 +1,27 @@
+using DeckScaler.Component;
+using DeckScaler.Scopes;
+using DeckScaler.Service;
+using Entitas;
+using Entitas.Generic;
+
+namespace DeckScaler.Systems
+{
+    public sealed class HideRecruitmentUIOnStageCompleted : IExecuteSystem
+    {
+        private readonly IGroup<Entity<Game>> _events
+            = Contexts.Instance.GetGroup(
+                MatcherBuilder<Game>
+                    .With<StageCompletedEvent>()
+                    .Without<Processed>()
+                    .Build()
+            );
+
+        private static GameplayHUD HUD => ServiceLocator.Resolve<IUiMediator>().GetCurrentScreen<GameplayHUD>();
+
+        public void Execute()
+        {
+            foreach (var _ in _events)
+                HUD.RecruitmentStageView.SetActive(false);
+        }
+    }
+}
