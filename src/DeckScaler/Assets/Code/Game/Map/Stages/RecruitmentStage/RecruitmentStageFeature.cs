ddf1c04f0ff06c4d3a57fd1d526c716c972c1858
rapuska <mikalajsiatkouski@gmail.com>
2024-12-28 16:05:53 +0300

    remove tmp system

--- src/DeckScaler/Assets/Code/Game/Map/Stages/RecruitmentStage/RecruitmentStageFeature.cs
+++ src/DeckScaler/Assets/Code/Game/Map/Stages/RecruitmentStage/RecruitmentStageFeature.cs
@@ -1,10 +1,4 @@
-using DeckScaler.Component;
 using DeckScaler.Systems;
-using Entitas;
-using Entitas.Generic;
-using UnityEngine;
-using Cursor = DeckScaler.Component.Cursor;
-using Input = DeckScaler.Scopes.Input;
 
 namespace DeckScaler
 {
@@ -19,40 +13,8 @@ namespace DeckScaler
             Add(new PlaceRecruits());
             Add(new DestroyAllRecruitsOnStageCompleted());
 
-            Add(new TEST());
-
             Add(new UpdateRecruitmentStageUI());
             Add(new HideRecruitmentUIOnStageCompleted());
-        }
-    }
-
-    public class TEST : IExecuteSystem
-    {
-        private readonly IGroup<Entity<Input>> _hoveredEntities
-            = Contexts.Instance.GetGroup(
-                MatcherBuilder<Input>
-                    .With<HoveredEntity>()
-                    .Build()
-            );
-        private readonly IGroup<Entity<Input>> _cursors
-            = Contexts.Instance.GetGroup(
-                MatcherBuilder<Input>
-                    .With<Cursor>()
-                    .And<CursorClicked>()
-                    .Build()
-            );
-
-        public void Execute()
-        {
-            foreach (var hovered in _hoveredEntities)
-            foreach (var _ in _cursors)
-            {
-                var entity = hovered.Get<HoveredEntity>().Value.GetEntity();
-
-                Debug.Log("click");
-                if (entity.Is<RecruitmentCandidate>())
-                    Debug.Log("click on recruit!");
-            }
         }
     }
 }
