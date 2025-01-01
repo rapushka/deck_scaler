11db3f2cb64cc7c4ebd77e5395ec3fbd8cade24d
rapuska <mikalajsiatkouski@gmail.com>
2025-01-01 15:05:24 +0300

    refactorings

--- src/DeckScaler/Assets/Code/Game/Map/_Feature/Utils/UnitsUtil.cs
+++ src/DeckScaler/Assets/Code/Game/Map/_Feature/Utils/UnitsUtil.cs
@@ -13,7 +13,7 @@ namespace DeckScaler
         private static UnitsConfig Config => ServiceLocator.Resolve<IConfigs>().Units;
 
         public Entity<Game> TakeToTeam(Entity<Game> unit)
-            => AddAllyBundle(
+            => AddAllyComponents(
                 unit
                     // from recruit
                     .Is<RecruitmentCandidate>(false)
@@ -23,7 +23,7 @@ namespace DeckScaler
                     .Is<UnitInShop>(false)
             );
 
-        public Entity<Game> AddAllyBundle(Entity<Game> unit)
+        public Entity<Game> AddAllyComponents(Entity<Game> unit)
             => unit
                 // add components, that makes him the teammate
                 .Is<Draggable>(true)
